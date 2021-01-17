1ebda53e0be52c571f2295b871a23f7e477a79eb
Yeison Suarez <yeisong12ayeisondavidsuarezg12@gmail.com>
2021-01-17 17:24:01 -0500

    First commit

---
+++ src/components/GifGridItem.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const GifGridItem = ( { url, title} ) => {
+
+    console.log(title, url);
+
+
+
+    return (
+        <div className="card animate__animated animate__fadeIn ">
+            <img src={ url } alt={ title } />
+            <p>{ title }</p>
+        </div>
+    )
+}
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,23 @@
+
+
+
+export const getGifs = async( category ) => {
+
+        
+
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=GMuncTFEMX1K9Aq8laSNPq5U6cjB3BI8`;
+    const resp = await fetch( url );
+    const {data} = await resp.json();
+
+    const gifs = data.map( img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    })
+    return gifs;
+
+
+
+}
